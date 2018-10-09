5f5645270a31916fdf02d80dc1395e19e3bfd782
Jacob Straszynski <jacob.straszynski@planet.com>
2018-10-08 19:40:26 -0700

    installation tweaks

--- contrib/update.sh
+++ contrib/update.sh
@@ -2,17 +2,19 @@
 
 # Update crusher code.
 cd /crush/crusher
-if [[ $(git rev-list HEAD...origin/master --count) -ne "0" ]]; then
-    git fetch && git reset --hard origin/master
-    cp -f contrib/upstart/crusher.conf /etc/init/crusher.conf
-    cp -f contrib/upstart/crusher.conf /etc/init/ngrok.conf
+if [[ $(sudo -u crusher git fetch && sudo git rev-list HEAD...origin/master --count) -ne "0" ]]; then
+    sudo -u crusher git fetch && sudo -u crusher git reset --hard origin/master
+    sudo cp -f contrib/upstart/crusher.conf /etc/init/crusher.conf
+    sudo cp -f contrib/upstart/ngrok.conf /etc/init/ngrok.conf
     sudo service crusher restart
+    sudo chown -R crusher:crusher /crush
 fi
 
 # Update reserver code.
 cd /crush/reserver
-if [[ $(git rev-list HEAD...origin/master --count) -ne "0" ]]; then
-    git fetch && git reset --hard origin/master
-    cp -f contrib/upstart/reserver.conf /etc/init/reserver.conf
+if [[ $(sudo -u crusher git fetch && sudo -u crusher git rev-list HEAD...origin/master --count) -ne "0" ]]; then
+    sudo -u crusher git fetch && sudo -u crusher git reset --hard origin/master
+    sudo cp -f contrib/upstart/reserver.conf /etc/init/reserver.conf
     sudo service reserver restart
+    sudo chown -R crusher:crusher /crush
 fi
