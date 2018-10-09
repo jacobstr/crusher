e5b05288512f65c41d224cd2bd0bee45f1e78a53
Jacob Straszynski <jacob.straszynski@planet.com>
2018-10-08 20:09:53 -0700

    s/source/./ since source is nonstandard

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This should export your SLACK_API_KEY.
-source ~/.private/slack
+. ~/.private/slack
 
 # Generic flask app startup.
-source venv/bin/activate
+. venv/bin/activate
 export FLASK_APP=app.py
 python -m flask run
