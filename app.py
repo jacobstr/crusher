5b29ad1ba2a7c88d088c8401d35394a36385cd5a
Jacob Straszynski <jacob.straszynski@planet.com>
2019-03-13 18:51:27 -0700

    updates for behavioral changes to the api.

--- app.py
+++ app.py
@@ -68,59 +68,92 @@ def run(watcher_id, date, length, campground):
     start_date = arrow.get(date, 'DD/MM/YY')
     end_date = start_date.shift(days=length)
 
+    # A sample site payload:
+    # {
+    #     "availabilities": {
+    #         "2018-10-05T00:00:00Z": "Reserved",
+    #         ...
+    #         "2018-10-20T00:00:00Z": "reserved"
+    #     },
+    #     "campsite_id": "99",
+    #     "campsite_reserve_type": "Site-Specific",
+    #     "loop": "UPPER PINES ",
+    #     "quantities": null,
+    #     "site": "043"
+    # }
     resp = requests.get(
-        'https://www.recreation.gov/api/camps/availability/campground/{id}'.format(id=campground['id']),
+        'https://www.recreation.gov/api/camps/availability/campground/{id}/month'.format(id=campground['id']),
+        headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'},
         params={
-            'start_date': start_date.format('YYYY-MM-DDTHH:mm:ssZZ'),
-            'end_date': end_date.format('YYYY-MM-DDTHH:mm:ssZZ'),
+            'start_date': start_date.format('YYYY-MM-01T00:00:00') + 'Z',
         }
     )
 
     if resp.status_code != 200:
         return []
 
-    def _rgov_site_has_availability(site):
-        """
-        Returns True if the recreation.gov `site` payload indicates an
-        availability.
-
-        The availabilities returned assume that a proper `start_date` was
-        provided. If so, the first element of the "availabilities" array should
-        begin at the `start_date` provided above. We truncate the availabilities
-        to
-
-        A sample site payload:
-
-            {
-                "availabilities": {
-                    "2018-10-05T00:00:00Z": "Reserved",
-                    ...
-                    "2018-10-20T00:00:00Z": "reserved"
-                },
-                "campsite_id": "99",
-                "campsite_reserve_type": "Site-Specific",
-                "loop": "UPPER PINES ",
-                "quantities": null,
-                "site": "043"
+    responses = [resp.json()]
+    # The api requires getting availabilities by month at a time. If we're
+    # looking for a reservation that spans multiple months, we need to look at
+    # multiple months worth of availabilities. We assume that no one is staying
+    # longer than one month and will make at most 2 requests.
+    if start_date.month != end_date.month:
+        resp = requests.get(
+            'https://www.recreation.gov/api/camps/availability/campground/{id}/month'.format(id=campground['id']),
+            params={
+                'start_date': end_date.format('YYYY-MM-01T00:00:00Z'),
             }
+        )
 
+        if resp.status_code != 200:
+            return []
+
+        responses.append(resp.json())
+
+    def _collect_sites(responses):
         """
-        for avdate, status in list(site['availabilities'].iteritems())[:length]:
+        Helps mangle multiple responses into a single index of availbilities by
+        site id.
+        """
+        availabilities_by_site = {}
+        for payload in responses:
+            for site_id, site in payload['campsites'].iteritems():
+                if not availabilities_by_site.get(site_id):
+                    availabilities_by_site[site_id] = {
+                        'site': site,
+                        'availabilities': {},
+                    }
+                availabilities_by_site[site_id]['availabilities'].update(site['availabilities'])
+        return availabilities_by_site
+
+    def _availability_fraction(site, start_date, end_date):
+        interested_dates = []
+        total_days = (end_date - start_date).days + 1
+        total_matched = 0
+        for avdate, status in list(site['availabilities'].iteritems()):
+            avparsed = arrow.get(avdate)
+            # Ignore dates outside of our interested range.
+            if not (avparsed >= start_date and avparsed <= end_date):
+                pass
             if status.lower() == 'available':
-                return True
-        return False
+                total_matched = total_matched + 1
+        return total_matched / total_days
 
     results = []
-    for site_id, site in resp.json()['campsites'].iteritems():
-        if _rgov_site_has_availability(site):
+    for site_id, site in _collect_sites(responses).iteritems():
+        availability_fraction = _availability_fraction(site, start_date, end_date)
+        if availability_fraction > 0:
             results.append({
                 "date": date,
                 "url": "https://www.recreation.gov/camping/campgrounds/{}/availability".format(campground['id']),
                 "campground": campground,
                 "campsite": site['site'],
+                "fraction": availability_fraction,
             })
 
-    return results
+    # Return the list of sites by their availability fraction of the dates
+    # desired since we prefer not to move campsites, but will if we have to.
+    return sorted(results, key=lambda site: site['fraction'], reverse=True)
 
 
 def mock_watchers():
