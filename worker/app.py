499355d95d3a63f50ba07356eefd005e51131165
Jacob Straszynski <jacob.straszynski@planet.com>
2019-06-13 23:04:49 -0700

    add timezones

--- worker/app.py
+++ worker/app.py
@@ -65,7 +65,7 @@ def get_watchers():
 
 
 def run(watcher_id, date, length, campground):
-    start_date = arrow.get(date, 'DD/MM/YY')
+    start_date = arrow.get(date, 'DD/MM/YY', tzinfo=cg['tz'])
     end_date = start_date.shift(days=length)
 
     # A sample site payload:
